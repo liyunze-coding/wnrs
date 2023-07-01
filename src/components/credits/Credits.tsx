9b14e6abbfd4f8c957399fa108b86652097437a2
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2023-07-01 16:39:00 +0800

    Update Credits.tsx

--- src/components/credits/Credits.tsx
+++ src/components/credits/Credits.tsx
@@ -1,20 +1,34 @@
 import { contStyles, creditStyles, creditTitleStyles } from "./Credits.css";
 
 const Credits = () => {
-  return (
-    <div className={creditStyles}>
-      <div className={creditTitleStyles}>how & who</div>
-      <div className={contStyles}>
-        <b>How to play:</b> <br />
-        Progress from level to level. Become more than strangers, one card at a time.
-        <p>Refresh to reset card decks.</p>{" "}
-        <p>
-          Made by <a href="https://github.com/munjoonteo">@munjoonteo</a> and{" "}
-          <a href="https://github.com/ilyues">@ilyues</a>.
-        </p>
-      </div>
-    </div>
-  );
+    return (
+        <div className={creditStyles}>
+            <div className={creditTitleStyles}>how & who</div>
+            <div className={contStyles}>
+                <b>How to play:</b> <br />
+                Progress from level to level. Become more than strangers, one card at a time.
+                <p>Refresh to reset card decks.</p>{" "}
+                <p>
+                    Made by{" "}
+                    <a href="https://github.com/munjoonteo" target="_blank">
+                        @munjoonteo
+                    </a>{" "}
+                    and{" "}
+                    <a href="https://github.com/ilyues" target="_blank">
+                        @ilyues
+                    </a>
+                    .
+                </p>
+                <p>
+                    Modified by{" "}
+                    <a href="https://github.com/liyunze-coding" target="_blank">
+                        @liyunze-coding
+                    </a>{" "}
+                    with Dark mode toggle.
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default Credits;
