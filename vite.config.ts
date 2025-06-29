67702fd948e946f4b073f5c29af6475b63b48685
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2025-06-29 13:14:11 +1000

    transformed into a PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,37 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
-});
+  plugins: [react(), vanillaExtractPlugin(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: "We're not really strangers",
+      short_name: 'wnrs',
+      description: 'Digital adaptation of a popular card game designed to foster meaningful connections through engaging questions and prompts',
+      theme_color: '#C13927',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
